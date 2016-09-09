1ebc0623bfe7d362fad78751135c802a359a69f3
Simon  Flachs <Simon@Simons-MacBook-Air-4.local>
2016-09-09 12:44:25 +0200

    xx

--- Week 2/pcpp-week02/TestCountFactors.java
+++ Week 2/pcpp-week02/TestCountFactors.java
@@ -1,22 +1,27 @@
 // For week 2
 // sestoft@itu.dk * 2014-08-29
+import java.util.concurrent.atomic.*;
 
 class TestCountFactors {
   public static void main(String[] args) {
 
-    MyAtomicInteger idx = new MyAtomicInteger();
- 
-    MyAtomicInteger count = new MyAtomicInteger();
+    //MyAtomicInteger idx = new MyAtomicInteger();
+    AtomicInteger idx = new AtomicInteger();
+
+    //MyAtomicInteger count = new MyAtomicInteger();
+    AtomicInteger count = new AtomicInteger();
+
     Thread[] threads = new Thread[10];
 
     for (int i = 0; i <= 9; i++) {
+
       System.out.println(idx.get());
       int val = 500_000*idx.get();
       int val1 = 500_000*(idx.get()+1);
       threads[i] = new Thread(() -> {
         System.out.println("Start: " + val + " " + val1);
         for(int x = val; x<val1; x++) {
-          count.addAndGet(countFactors(x));
+          count.addAndGet(countFactors(x)); 
         }
       });
       idx.addAndGet(1);
