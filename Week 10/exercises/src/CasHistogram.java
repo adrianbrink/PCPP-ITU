f0ec06d56b2d138b109f76b31dc9ce116caeacfe
Adrian Brink <adrian@brink-holdings.com>
2016-11-11 10:18:42 +0100

    setup'

--- Week 10/exercises/src/CasHistogram.java
+++ Week 10/exercises/src/CasHistogram.java
@@ -1,7 +1,7 @@
 /**
  * Created by adrianbrink on 11/11/2016.
  */
-public class Test {
+public class CasHistogram {
     public static void main(String[] args) {
         System.out.print("ss");
     }
